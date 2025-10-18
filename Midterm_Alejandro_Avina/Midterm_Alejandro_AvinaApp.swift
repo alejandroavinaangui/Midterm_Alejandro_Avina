7ad92251e16e754009cc04972a1fc8378d88f477
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-10-18 10:24:21 -0700

    Initial Commit

---
+++ Midterm_Alejandro_Avina/Midterm_Alejandro_AvinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midterm_Alejandro_AvinaApp.swift
+//  Midterm_Alejandro_Avina
+//
+//  Created by Alejandro Avina on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Midterm_Alejandro_AvinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
