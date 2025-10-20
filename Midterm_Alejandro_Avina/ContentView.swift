596b0b65d64e15275bec4dfde1b08dcae1b396d5
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-10-19 21:59:06 -0700

    Updates

--- Midterm_Alejandro_Avina/ContentView.swift
+++ Midterm_Alejandro_Avina/ContentView.swift
@@ -4,18 +4,46 @@
 //
 //  Created by Alejandro Avina on 10/18/25.
 //
+// Midterm
+// Name: Alejandro Avina Date: 10/18/2025
 
 import SwiftUI
 
+
+let details = Bundle.main.decode([Subject].self,from:"Data.json")
+
+struct DetailView: View {
+    var body: some View {
+        
+    }
+}
+
+
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            List{
+                ForEach(details){
+                    section in
+                    NavigationLink(destination:{
+                        VStack{
+                            Image(section.imageName)
+                            .resizable()
+                            .frame(height:400)
+                            Text(section.description).padding()
+                        }
+                    }
+                    ){
+                        HStack{
+                            Image(section.imageName).resizable().frame(width:50,height:50)
+                            Text(section.name)
+                        }
+                    }
+                }.navigationTitle("Alejandro's tool reference")
+            }
         }
-        .padding()
     }
 }
 
